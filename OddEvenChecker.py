95363deba2bc848f02a95d176e0940a6c4490cae
Amit Nijsure <amitnijsure1234@gmail.com>
2017-09-13 12:51:10 +0530

    Added OddEvenChecker program

---
+++ OddEvenChecker.py
@@ -0,0 +1,6 @@
+num = int(input("\nEnter the number to be checked : "))
+
+if num % 2 == 0:
+    print("\n" + str(num) + " is an Even Number.\n")
+else:
+    print("\n" + str(num) + " is an Odd Number.")
