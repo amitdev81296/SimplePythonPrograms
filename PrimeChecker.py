703f50b19d91b1a7d1c399c27d486324c4a9e040
Amit Nijsure <amitnijsure1234@gmail.com>
2017-09-13 12:47:05 +0530

    Added Prime Checker program

--- PrimeChecker.py
+++ PrimeChecker.py
@@ -1,10 +1,14 @@
 num = int(input("\nEnter a number to be checked : "))
 flag = 0
+divisible_by = [1]
 for i in range(2, int(num/2) + 1):
     if (num % i) == 0:
+        divisible_by.append(i)
         flag = 1
 
+divisible_by.append(num)
+
 if flag == 1:
-    print("\n" + str(num) + " is not a prime number.")
+    print("\n" + str(num) + " is not a prime number. \nDivisible by : " + str(divisible_by))
 else:
     print("\n" + str(num) + " is prime number.")
